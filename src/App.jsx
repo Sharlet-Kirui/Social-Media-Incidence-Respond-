b36762339b6bdf1d9451c2a87016e3415db5fc08
Gabriel Were <weregabriel91@gmail.com>
2026-01-27 08:49:56 +0300

    add gvt officials page

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,14 @@ import LinkedInIncidents from './frontend/components/IncidentLinkedIn';
 import Profile from './frontend/components/Profile';
 import Verifications from './frontend/components/Verifications/xverifications';
 import Metaverifications from './frontend/components/Verifications/Metaverifications';
+<<<<<<< HEAD
 import IncidentX from './frontend/components/IncidentX';
 import IncidentTelegram from './frontend/components/IncidentTelegram';
 import IncidentTikTok from './frontend/components/IncidentTikTok';
 import IncidentMeta from './frontend/components/IncidentMeta';
+=======
+import Officials from './frontend/components/GovernmentOfficials/Officials';
+>>>>>>> 1963361 (add gvt officials page)
 
 function App() {
   return (
@@ -44,8 +48,14 @@ function App() {
           <Route path="/verification/X" element={<Verifications/>} />
           <Route path="/compromisedaccounts" element= {<CompromisedAccounts />} />
           <Route path="/latestResponded" element={<h1>Latest Responded</h1>} />
+<<<<<<< HEAD
           <Route path="/governmentOfficials" element={<h1>Government Officials</h1>} />
           <Route path="/profile" element={<Profile />} />
+=======
+          <Route path="/governmentOfficials" element={<Officials/>} />
+          <Route path="/Profile" element={<Profile />} />
+
+>>>>>>> 1963361 (add gvt officials page)
         </Routes>
       </div>
     </Router>
