5a97b434b28c9555fd6c3bc38a0d04a19db70762
gabrielwere <weregabriel91@gmail.com>
2026-02-09 12:04:26 +0300

    whatsapp excel file upload

--- src/frontend/components/Incidents/IncidentWhatsApp.jsx
+++ src/frontend/components/Incidents/IncidentWhatsApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect,useState } from 'react';
 import { Plus, Edit2, MoreHorizontal, Trash } from "lucide-react";
 import '../global.css';
 
@@ -7,6 +7,7 @@ const emptyForm = {
   dateReported: "",
   status: "Rejected",
   officer: "",
+  incident:"",
 };
 
 const IncidentWhatsApp = () => {
@@ -16,6 +17,10 @@ const IncidentWhatsApp = () => {
   const [formData, setFormData] = useState(emptyForm);
   const [editingIndex, setEditingIndex] = useState(null);
   const [menuIndex, setMenuIndex] = useState(null);
+  const [fileChosen,setFileChosen] = useState(false)
+  const [displayFileForm,setDisplayFileForm] = useState(true)
+  const [filePath,setFilePath] = useState("")
+  const fileData = new FormData()
   
   // --- Search & Filter States ---
   const [searchTerm, setSearchTerm] = useState("");
@@ -27,6 +32,18 @@ const IncidentWhatsApp = () => {
   const rejectedCount = rows.filter(r => r.status === "Rejected").length;
   const pendingCount = rows.filter(r => r.status === "Pending").length;
   const resolvedCount = rows.filter(r => r.status === "Resolved").length;
+
+  const WHATSAPP_INCIDENTS_URL = "http://localhost:4000/whatsapp-incidents/"
+  const FILE_UPLOAD_URL = "http://localhost:4000/whatsapp-incidents/upload-file"
+
+  useEffect(() => {
+    fetch(WHATSAPP_INCIDENTS_URL)
+    .then((response) => response.json())
+    .then((incidents) => setRows(incidents))
+    .catch((error) => {
+      console.log(error);
+    });
+  }, []);
 
   // --- Handlers ---
   const openCreate = () => {
@@ -48,6 +65,18 @@ const IncidentWhatsApp = () => {
       updated[editingIndex] = formData;
       setRows(updated);
     } else {
+      fetch(WHATSAPP_INCIDENTS_URL
+        , {
+        method: "POST",
+        headers: {
+          "Content-Type": "Application/JSON",
+        },
+        body: JSON.stringify(formData),
+      })
+      .then((response) => response.json())
+      .catch((error) => {
+        console.log(error);
+      });
       setRows([...rows, formData]);
     }
     setIsModalOpen(false);
@@ -66,6 +95,31 @@ const IncidentWhatsApp = () => {
       status: tempFilterStatus
     });
   };
+
+  const addRecords = () => {
+
+    if(filePath !== ""){
+
+      fileData.append("incidents-file",filePath)
+      
+      fetch(FILE_UPLOAD_URL
+        , {
+        method: "POST",
+        body: fileData,
+      })
+      .then((response) => response.json())
+      .catch((error) => {
+        console.log(error);
+      });
+
+      setFileChosen(false)
+      setDisplayFileForm(true)
+
+    }else{
+      alert("Upload file first")
+    }
+    
+  }
 
   const filteredRows = rows.filter(row => {
     const matchesSearch = row.mobileNumber.toLowerCase().includes(searchTerm.toLowerCase());
@@ -158,6 +212,35 @@ const IncidentWhatsApp = () => {
             <Plus size={18} /> Add New Incident
           </button>
         </div>
+
+        <div className="right-actions">
+          <form>
+            {displayFileForm &&
+              <>
+               <label htmlFor="x-incidents-file" className="btn-primary"><Plus size={18} />Upload from Excel</label>
+               <input type="file" id="x-incidents-file" hidden={true} onChange={()=> {
+                if(document.getElementById("x-incidents-file").value !== ""){
+                  setFilePath(event.target.files[0])
+                  setFileChosen(true)
+                  setDisplayFileForm(false)
+                }
+              }
+              }/>
+              </>
+            }
+            
+            
+            {fileChosen &&
+              <>
+              <input type="reset" value={"Drop " + filePath.name} className="btn-drop-file" onClick={()=>{
+                setFileChosen(false)
+                setDisplayFileForm(true)
+              }}/>
+              <button type="button" className="btn-submit-file" onClick={addRecords}> Add Records </button>
+              </>
+            }
+          </form>
+        </div>
       </div>
 
       {/* 4. Table Section */}
@@ -166,6 +249,7 @@ const IncidentWhatsApp = () => {
           <div className="th-cell col-wa-no">No</div>
           <div className="th-cell col-wa-mobile">Mobile Number</div>
           <div className="th-cell col-wa-date">Date Reported</div>
+          <div className="th-cell col-wa-date">Incident</div>
           <div className="th-cell col-wa-status">Status</div>
           <div className="th-cell col-wa-officer">Officer</div>
           <div className="th-cell col-wa-action">Action</div>
@@ -178,6 +262,7 @@ const IncidentWhatsApp = () => {
               <div className="td-cell col-wa-no">{i + 1}</div>
               <div className="td-cell col-wa-mobile">{row.mobileNumber}</div>
               <div className="td-cell col-wa-date">{row.dateReported}</div>
+              <div className="td-cell col-wa-date">{row.incident}</div>
               <div className="td-cell col-wa-status">
                 <span className={`status-badge ${
                     row.status === "Rejected" ? "status-rejected" : 
@@ -240,6 +325,24 @@ const IncidentWhatsApp = () => {
                   value={formData.dateReported}
                   onChange={(e) => setFormData({ ...formData, dateReported: e.target.value })}
                 />
+              </div>
+
+              <div className="form-group">
+                <label>Incident</label>
+                <select value={formData.incident} onChange={(e) => setFormData({ ...formData, incident: e.target.value })}>
+                  <option value="Impersonation">Impersonation</option>
+                  <option value="Publication of False Information">Publication of False Information</option>
+                  <option value="Account Compromise">Account Compromise</option>
+                  <option value="E-Commerce Fraud">E-Commerce Fraud</option>
+                  <option value="Online Sextortion">Online Sextortion</option>
+                  <option value="Cyber Harassment">Cyber Harassment</option>
+                  <option value="Online Child Exploitation">Online Child Exploitation</option>
+                  <option value="Cyber Terrorism">Cyber Terrorism</option>
+                  <option value="Data Breach">Data Breach</option>
+                  <option value="Copyright Infringement">Copyright Infringement</option>
+                  <option value="Wrongful Suspension">Wrongful Suspension</option>
+                  <option value="Hate Speech">Hate Speech</option>
+                </select>
               </div>
 
               <div className="form-group">
